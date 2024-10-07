8d54eb4d491273921d04c8bc24e32e4ed13d689a
Indu Shekhar Jha <126399927+indu-shekhar@users.noreply.github.com>
2024-10-07 10:33:20 +0000

    animating the introductory statement

--- script.js
+++ script.js
@@ -4,15 +4,46 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-const allowedUrls = ['projects.html', 'blog.html', 'education.html'];
+const allowedUrls = ["projects.html", "blog.html", "education.html"];
 
 function navigateTo(page) {
   if (allowedUrls.includes(page)) {
     window.location.href = page;
   } else {
-    console.error('Invalid URL');
+    console.error("Invalid URL");
   }
 }
+
+//animating the paragraph about what i do
+const roles = [
+  "Frontend Developer",
+  "Backend Developer",
+  "Fullstack Developer",
+  "Software Engineer",
+];
+let currentRoleIndex = 0;
+const textElement = document.getElementById("animated-text");
+
+function typeRole(role) {
+  textElement.style.animation = `typing 3s steps(${role.length}), blink-caret .75s step-end infinite`;
+  textElement.textContent = role;
+}
+function deleteRole() {
+  textElement.style.animation = `deleting 3s steps(${textElement.textContent.length}), blink-caret .75s step-end infinite`;
+  textElement.textContent = "";
+}
+function cycleRoles() {
+  typeRole(roles[currentRoleIndex]);
+  setTimeout(() => {
+    deleteRole();
+    setTimeout(() => {
+      currentRoleIndex = (currentRoleIndex + 1) % roles.length;
+      cycleRoles();
+    }, 3000);
+  }, 3000);
+}
+cycleRoles();
+
 // anime({
 //   targets: 'div',
 //   translateX: 250,
